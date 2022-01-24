10061b18769a154986042394d1388f7c23310ae2
Hamzasehouli <sehoulihamza@yahoo.com>
2022-01-24 19:45:55 +0100

    Installed react native into client folder

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    /////SIGNUP
+    public function signup(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'firstname' => ['required', 'string'],
+            'lastname' => ['required', 'string'],
+            'email' => ['required', 'email', 'string'],
+            'password' => ['required', 'string'],
+            'phone' => ['required', 'string', 'min:10'],
+        ]);
+
+        $user = User::create([
+            'firstname' => $validated['firstname'],
+            'lastname' => $validated['lastname'],
+            'email' => $validated['email'],
+            'password' => Hsah::make($validated['password'], [
+                'rounds' => 12,
+            ]),
+            'phone' => $validated['phone'],
+        ]);
+
+        $token = $user->createToken('myToken')->plainTextToken;
+
+        return response([
+            'status' => 'success',
+            'data' => [
+                'user' => $user,
+            ],
+            'token'=>$token
+        ], 201);
+
+    }
+
+    ///////LOGIN
+    public function login(Request $request)
+    {
+
+    }
 }
